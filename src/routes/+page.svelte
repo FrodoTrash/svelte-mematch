6409e561b5d658b7415f2f353c12c643ffa93085
FrodoTrash <mheybal@gmail.com>
2023-08-15 20:28:24 +0200

    working timer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,11 +28,35 @@
 		if (grid[first] == grid[second]) {
 			matches = matches.concat(grid[first]);
 		}
+		//TODO you can't speedrun if there's a setTimeout delay
 		setTimeout(() => (selected = []), 500);
+	};
+
+	const startGameTimer = () => {
+		const countdown = () => {
+			time -= 1;
+		};
+		timerId = setInterval(countdown, 1000);
+	};
+
+	const resetGame = () => {
+		timerId && clearInterval(timerId);
+		grid = createGrid();
+		maxMatches = grid.length / 2;
+		selected = [];
+		matches = [];
+		timerId = null;
+		time = 20;
 	};
 
 	const gameWon = () => {
 		state = 'won';
+		resetGame();
+	};
+
+	const gameLost = () => {
+		state = 'lost';
+		resetGame();
 	};
 
 	let state: State = 'start';
@@ -41,14 +65,21 @@
 	let maxMatches = grid.length / 2;
 	let selected: number[] = [];
 	let matches: string[] = [];
+	let timerId: number | null | undefined | NodeJS.Timeout = null;
+	let time: number = 20;
+
+	$: if (state == 'playing') {
+		!timerId && startGameTimer();
+	}
 
 	$: selected.length == 2 && matchCards();
 	$: maxMatches == matches.length && gameWon();
+	$: time == 0 && gameLost();
 
 	$: console.log(state, selected, matches);
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container h-full mx-auto flex justify-center items-center text-center">
 	<div class="space-y-5 grid">
 		{#if state == 'start'}
 			<h1 class="h1">MeMatch Game</h1>
@@ -56,8 +87,26 @@
 				>Play</button
 			>
 		{/if}
-		<!-- h-auto max-w-full rounded-lg -->
+
 		{#if state == 'playing'}
+			<h1
+				class="h1"
+				class:animate-jump={time <= 10}
+				class:animate-infinite={time <= 10}
+				class:animate-duration-1000={time <= 10}
+				class:animate-ease-in-out={time <= 10}
+			>
+				{time}
+			</h1>
+
+			<div class="flex space-x-4">
+				{#each matches as card}
+					<div>
+						<h2 class="h1">{card}</h2>
+					</div>
+				{/each}
+			</div>
+
 			<section class="grid grid-cols-5 gap-4">
 				{#each grid as card, cardIndex}
 					<!-- local const's -->
@@ -67,7 +116,8 @@
 
 					<button
 						class="card p-12 card-hover text-7xl"
-						class:selected={isSelected}
+						class:variant-glass-error={isSelected}
+						class:variant-glass-primary={match}
 						disabled={isSelectedOrMatch}
 						on:click={() => selectCard(cardIndex)}
 					>
@@ -94,11 +144,16 @@
 </div>
 
 <style>
-	.selected {
-		border: 2px solid gold;
+	.variant-glass-error {
+		transition: opacity 0.5s ease-in-out;
+		opacity: 0.7;
 	}
 	.match {
 		transition: opacity 1.5s ease-out;
-		opacity: 0.4;
+		opacity: 0.3;
+	}
+	.variant-glass-primary {
+		transition: opacity 1.5 ease-in;
+		opacity: 0.8;
 	}
 </style>
